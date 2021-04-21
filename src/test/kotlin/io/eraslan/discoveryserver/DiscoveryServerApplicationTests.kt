6473cb93a88bb01c6d73079be0d459a87f5a9ffe
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2021-04-21 12:19:02 +0200

    first commit

---
+++ src/test/kotlin/io/eraslan/discoveryserver/DiscoveryServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.eraslan.discoveryserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
